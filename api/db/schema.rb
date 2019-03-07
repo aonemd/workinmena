ede265f8d0325934fc5a4f7e9cb7f64ace242463
aonemd <asalehmo7@gmail.com>
2019-03-07 12:13:48 +0200

    Add database not null constraint and index on company name

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -16,9 +16,10 @@ ActiveRecord::Schema.define(version: 2019_03_06_160119) do
   enable_extension "plpgsql"
 
   create_table "companies", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_companies_on_name"
   end
 
 end
