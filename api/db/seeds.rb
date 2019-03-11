a5fd906b36a3b2f73e3077046a3a63a93d562257
aonemd <asalehmo7@gmail.com>
2019-03-11 14:26:25 +0200

    Add programming languages seed data

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -2,3 +2,12 @@ tool_categories = ActiveSupport::JSON.decode(File.read('db/seeds/tool_categories
 tool_categories.each do |tool_category|
   Tool::Category.create(name: tool_category['name'])
 end
+
+programming_language_tools = ActiveSupport::JSON.decode(File.read('db/seeds/programming_languages.json'))
+language_category          = Tool::Category.first
+programming_language_tools.each do |programming_language|
+  Tool.create(name: programming_language['name'],
+              website: programming_language['website'],
+              description: programming_language['description'],
+              category: language_category)
+end
