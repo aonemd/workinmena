512aa292cbb164046fba0b3318570bfbc7da18c5
aonemd <asalehmo7@gmail.com>
2019-03-29 19:30:58 +0200

    Fix company tools relationship
    By using the proper `through` relationship name, `stack`

--- api/app/models/company.rb
+++ api/app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
   has_many :stack, foreign_key: :company_id, class_name: 'Stack'
-  has_many :tools, through: :stacks
+  has_many :tools, through: :stack
 
   validates_presence_of :name
   validates_uniqueness_of :name
