3efd3354abf09c3e4833c55ff134cb184e810e02
aonemd <asalehmo7@gmail.com>
2019-03-29 19:15:56 +0200

    Fix type in stack uniqueness error message

--- api/app/models/stack.rb
+++ api/app/models/stack.rb
@@ -2,5 +2,5 @@ class Stack < ApplicationRecord
   belongs_to :company
   belongs_to :tool
 
-  validates_uniqueness_of :company_id, scope: :tool_id, message: "Tool alreaded added to company's stack"
+  validates_uniqueness_of :company_id, scope: :tool_id, message: "Tool already added to company's stack"
 end
