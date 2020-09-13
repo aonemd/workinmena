0dcfff2ad4b7795ef9886e8e7cae89cfb2f08325
aonemd <asalehmo7@gmail.com>
2020-09-13 18:02:14 +0200

    Return id along with decorated company

--- api/app/decorators/company_decorator.rb
+++ api/app/decorators/company_decorator.rb
@@ -1,6 +1,7 @@
 class CompanyDecorator < Geckorate::Decorator
   def decorate(options = {})
     {
+      id: id,
       name: name,
       website: website,
       tools: ToolDecorator.decorate_collection(tools)
