475a0655097445607d687a1df5ccc2c728e26369
aonemd <asalehmo7@gmail.com>
2020-09-15 19:40:28 +0200

    Add styles for company page

--- frontend/src/components/Company.vue
+++ frontend/src/components/Company.vue
@@ -1,8 +1,17 @@
 <template>
-  <h1>{{state.company.name}}</h1>
-  <span><b>Website:</b> {{state.company.website}}</span>
-  <br>
-  <span><b>Tools:</b> {{state.tools}}</span>
+  <div id="company-box">
+    <div class="company-info">
+      <span class="company-title">{{state.company.name}}</span>
+
+      <a :href="'//' + state.company.website" target="_blank" class="company-website">
+        {{state.company.website}}
+      </a>
+
+      <ul v-for="tool in state.company.tools">
+        <li>{{tool.name}}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -36,3 +45,41 @@ export default defineComponent({
   }
 });
 </script>
+<style>
+#company-box {
+  width: 100%;
+  position: relative;
+  padding: 0;
+
+  background: var(--white);
+  border: 1px solid var(--border);
+  border-radius: 0.3em;
+  padding: 1em;
+  margin-bottom: 1em;
+  z-index: 1;
+  display: table; /* so the elements inside act like table cells so that they can be vertically aligned in the middle */
+  width: 96%;
+
+  & a {
+    text-decoration: none;
+  }
+
+  & .company-info {
+    line-height: 1.25em;
+    padding: 0 25px;
+    vertical-align: middle;
+
+    & .company-title {
+      font-weight: 700;
+      font-size: 1.5em;
+      color: var(--main);
+    }
+
+    & .company-website {
+      float: right;
+      padding-top: 0.4em;
+      color: var(--secondary);
+    }
+  }
+}
+</style>
