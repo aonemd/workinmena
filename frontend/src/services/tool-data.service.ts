10a7cac79c69f86630f0e84fe263eb1b138002f3
aonemd <asalehmo7@gmail.com>
2020-10-18 14:37:52 +0200

    Add ToolDataService `getAll()` consumer

---
+++ frontend/src/services/tool-data.service.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+import { Tool } from '../interfaces';
+
+const http = axios.create({
+  baseURL: 'http://localhost:3000/api/v1',
+  responseType: 'json',
+  headers: { 'Content-Type': 'application/json' }
+});
+
+class ToolDataService {
+  async getAll(): Promise<{tools: Tool[]}> {
+    const response = await http.get('/tools');
+    const {data}   = response;
+
+    return data;
+  }
+}
+
+export default new ToolDataService();
