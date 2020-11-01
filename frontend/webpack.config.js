c651f7426d075a464f3355d4e77656cdd71446f6
aonemd <asalehmo7@gmail.com>
2020-11-01 20:00:51 +0200

    Add yarn build command

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = (env = {}) => ({
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: "index.js",
-    publicPath: '/dist/'
+    publicPath: 'dist/'
   },
   module: {
     rules: [
