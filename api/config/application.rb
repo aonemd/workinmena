c4264b48e29498a223a6bcbcc8b96bfbce6af334
aonemd <asalehmo7@gmail.com>
2020-11-06 20:46:02 +0200

    Add `localhost` to allowed origins

--- api/config/application.rb
+++ api/config/application.rb
@@ -41,7 +41,7 @@ module Workinmena
 
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins ['http://localhost:8080']
+        origins ['http://localhost:8080', 'http://localhost']
         resource '*', headers: :any, methods: [:get, :post, :patch, :put]
       end
     end
