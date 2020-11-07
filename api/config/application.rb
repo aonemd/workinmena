e453435de97f239bb63f66f1464d35d67d7d6087
aonemd <asalehmo7@gmail.com>
2020-11-07 20:01:32 +0200

    Update allowed origins for production

--- api/config/application.rb
+++ api/config/application.rb
@@ -41,7 +41,7 @@ module Workinmena
 
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins ['http://localhost:8080', 'http://localhost']
+        origins ['http://localhost:8080', 'http://localhost', 'http://167.99.130.51']
         resource '*', headers: :any, methods: [:get, :post, :patch, :put]
       end
     end
