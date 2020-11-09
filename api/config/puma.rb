ba3b188fa1c8bb1483ecdc08c71d59f94ce62dbc
aonemd <asalehmo7@gmail.com>
2020-11-09 22:33:39 +0200

    Use tcp ssl socket

--- api/config/puma.rb
+++ api/config/puma.rb
@@ -10,7 +10,7 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -43,8 +43,8 @@ state_path "#{puma_dir}/pids/puma.state"
 activate_control_app
 
 # socket
-bind "unix://#{puma_dir}/sockets/puma.sock"
-# bind 'ssl://127.0.0.1:3000?key=/etc/letsencrypt/live/workinmena.tech/privkey.pem&cert=/etc/letsencrypt/live/workinmena.tech/fullchain.pem'
+# bind "unix://#{puma_dir}/sockets/puma.sock"
+bind 'ssl://127.0.0.1:3000?key=/etc/letsencrypt/live/workinmena.tech/privkey.pem&cert=/etc/letsencrypt/live/workinmena.tech/fullchain.pem'
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
