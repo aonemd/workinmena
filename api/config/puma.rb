a366f0aefecb476300b2285d0c125190a380e074
aonemd <asalehmo7@gmail.com>
2020-11-10 09:12:08 +0200

    Add ssl_bind besides unix bind

--- api/config/puma.rb
+++ api/config/puma.rb
@@ -4,17 +4,17 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch('PORT') { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch('RAILS_ENV') { 'development' }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -31,7 +31,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #
 # preload_app!
 
-root     = File.expand_path("../..", __FILE__)
+root     = File.expand_path('..', __dir__)
 puma_dir = "#{root}/puma"
 
 # logs
@@ -45,9 +45,9 @@ activate_control_app
 # socket
 bind "unix://#{puma_dir}/sockets/puma.sock"
 # bind 'ssl://127.0.0.1:3000?key=/etc/letsencrypt/live/workinmena.tech/privkey.pem&cert=/etc/letsencrypt/live/workinmena.tech/fullchain.pem'
-ssl_bind "127.0.0.1", "3002", {
-  key: "/etc/letsencrypt/live/workinmena.tech/privkey.pem",
-  cert: "cert=/etc/letsencrypt/live/workinmena.tech/fullchain.pem"
+ssl_bind '127.0.0.1', '3002', {
+  key: '/etc/letsencrypt/live/workinmena.tech/privkey.pem',
+  cert: 'cert=/etc/letsencrypt/live/workinmena.tech/fullchain.pem'
 }
 
 # Allow puma to be restarted by `rails restart` command.
